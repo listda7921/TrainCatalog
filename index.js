14e48527885fe872df87306366e8868860233039
dan <listda7921@gmail.com>
2017-07-21 13:23:19 +0000

    dirname

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  // var data = base64_encode(__dirname + "/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
+  // console.log(data);
   response.render('pages/index');
 });
 
@@ -34,11 +36,12 @@ app.get('/db', function (request, response) {
       else{
       // result.forEach(function(r){
       //   if(r.url  == "/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg"){
-      //     data = base64_encode("/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
+      //     data = base64_encode(__dirname + "/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
       //   }
       // })
-      //data = base64_encode("/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
-      response.render('pages/db', {results: result.rows }); 
+      data = base64_encode(__dirname + "/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
+      console.log(data);
+      response.render('pages/db', {results: data }); 
       }
        
     });
