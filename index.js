5ecb312c2ace83632965cc44de56b9ae18bb456c
dan <listda7921@gmail.com>
2017-07-21 05:16:53 +0000

    get2

--- index.js
+++ index.js
@@ -33,11 +33,13 @@ app.get('/db', function (request, response) {
        { console.error(err); response.send("Error " + err); }
       else{
       result.forEach(function(r){
-        if(r.url  == "/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
-        data = base64_encode("/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
+        if(r.url  == "/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg"){
+          data = base64_encode("/img/6c5f4840-6dc8-11e7-be82-59533fcdbf61.jpg")
+        }
       })
+      response.render('pages/db', {results: data} ); 
       }
-       { response.render('pages/db', {results: data} ); }
+       
     });
   });
 });
