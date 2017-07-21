c2c866dd6fed165c6d3ef7eab86da301bde2153c
dan <listda7921@gmail.com>
2017-07-21 04:50:00 +0000

    query

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/api/Upload', function(req, res){
     console.log(err);
   });
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('INSERT INTO image_locations(url) VALUES(' + path + ')', function(err, result) {
+    client.query('INSERT INTO image_locations(url) VALUES("' + path + '")', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
