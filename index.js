1754f1f7425b5b94c1fbd18a0e79f4a349a0bc45
dan <listda7921@gmail.com>
2017-07-21 04:04:01 +0000

    mobile

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var fs = require("fs");
+var uuid = require('uuid');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -29,3 +35,28 @@ app.get('/db', function (request, response) {
     });
   });
 });
+
+app.post('/api/Upload', function(req, res){
+  var img = decodeBase64Image(req.body.base64String);
+  var fileName = uuid.v1();
+  fs.writeFile(__dirname + '/img/' + fileName + '.jpg', img.data, function(err) {
+    console.log(err);
+  });
+  res.send("ok");
+})
+
+
+
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+    response = {};
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+}
